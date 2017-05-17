aeeda9aaaf97357529c214fef8e26dc66e390d91
JaneJiangHH <july_j_hut@hotmail.com>
2017-05-17 18:40:16 +0100

    no meaning

--- project/src/pages/MeaningPage.vue
+++ project/src/pages/MeaningPage.vue
@@ -17,6 +17,45 @@
         </p>
       </div>
     </section>
+    <section class="section no-top-padding" :class="{ isMobile: isMobile }" v-show="name === ''">
+      <div class="columns">
+        <div class="column is-8 is-offset-2">
+          <article class="message is-primary" id="msg-box">
+            <div class="message-header">
+              <p>Find more about my name</p>
+            </div>
+            <div class="message-body">
+              Type in your name in the search box above!
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section class="section no-top-padding" :class="{ isMobile: isMobile }" v-show="name !== '' && noMeaning">
+      <div class="columns">
+        <div class="column is-8 is-offset-2">
+          <article class="message is-warning" id="msg-box">
+            <div class="message-header">
+              <p>Name Not Found</p>
+            </div>
+            <div class="message-body" style="text-align: left;">
+              Sorry {{name.toUpperCase()}}, there is no further information about the name.
+              <br>
+              <small>
+                Try this page <a href="#">Find my name</a> and explore more about your name!
+              </small>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section class="section" :class="{ isMobile: isMobile}">
+      <result-and-share>
+        {{name.toUpperCase()}}, share your name on
+        <br> 
+        <br>
+      </result-and-share>
+    </section>
   </div>
 </template>
 
@@ -25,10 +64,24 @@ import NavBar from '../components/NavBar'
 import Banner from '../components/Banner'
 import MobileDetect from 'mobile-detect'
 import ResultAndShare from '../components/ResultAndShare'
+import meanset from '../assets/nameMeanings'
 export default {
   name: 'meaning-page',
   components: {
     NavBar, Banner, ResultAndShare
+  },
+  watch: {
+    name () {
+      const nameLowerCase = this.name.toLowerCase()
+      const findMean = Object.keys(meanset).filter(function (n) {
+        return n === nameLowerCase
+      })
+      if (findMean.length === 0) {
+        this.noMeaning = true
+      } else {
+        this.noMeaning = false
+      }
+    }
   },
   data () {
     return {
@@ -43,7 +96,10 @@ export default {
   },
   methods: {
     onChange () {
-      window.location.href = 'http://localhost:8080/#/meaning/' + this.name.toLowerCase()
+      if (this.noMeaning === false) {
+        window.location.href = 'http://localhost:8080/#/meaning/' + this.name.toLowerCase()
+        window.location.reload()
+      }
       // this.$router.push({name: 'meaning-name', params: {name: this.name.toLowerCase()}})
     }
   }
